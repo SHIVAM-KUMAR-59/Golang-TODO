f11b7086a9a01b9ac0adf5477893733732e814aa
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-04-04 23:41:53 +0530

    Model update

--- server/models/todo.go
+++ server/models/todo.go
@@ -2,6 +2,7 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
+// User model
 type Todo struct {
 	Id primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Title string `json:"title" binding:"required"`
