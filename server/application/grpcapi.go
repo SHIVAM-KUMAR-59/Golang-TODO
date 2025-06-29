14a36e4cd98daeef3b45a50b5123543f8c31ae61
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-06-29 15:57:51 +0530

    Frontend init + backend lil lil

--- server/application/grpcapi.go
+++ server/application/grpcapi.go
@@ -1,1 +1,37 @@
 package application
+
+import (
+	"context"
+
+	"shivam.com/server/application/service/types"
+	"shivam.com/server/application/service/types/entity"
+	"shivam.com/server/gen/go/application/models"
+)
+
+type grpcApi struct {
+	userService   types.UserService
+}
+
+func (g *grpcApi) CreateUser(ctx context.Context, in *models.CreateUserRequest) (*models.CreateUserResponse, error) {
+	name := in.Name
+	email := in.Email
+	password := in.Password
+	user := entity.User {
+		Name:     name,
+		Email:    email,
+		Password: password,
+	}
+
+	userId, err := g.userService.CreateUser(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.CreateUserResponse{Id: userId}, nil
+}
+
+func NewGrpcApi(userService types.UserService) *grpcApi {
+	return &grpcApi{
+		userService: userService,
+	}
+}
